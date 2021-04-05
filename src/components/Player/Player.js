31c7a94ddc688233ef29886aa46be006a1766c94
Marco Túlio <marcotmotta@gmail.com>
2021-04-05 20:00:32 -0300

    bottom player

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react'
 
 import './Player.scss';
 
+import { Avatar, Slider } from '@material-ui/core';
+import { VolumeUp, PlayCircleFilled } from '@material-ui/icons';
+
 export default class Player extends Component {
     render() {
         return (
             <div className="player">
-                <div className="song"></div>
-                <div className="player-butttons"></div>
-                <div className="options"></div>
+                <div className="song">
+                    <Avatar className="avatar" variant="square"></Avatar>
+                    <div>
+                        <div className="song-name">Blinding Lights</div>
+                        <div className="artist">The Weeknd</div>
+                    </div>
+                </div>
+                <div className="player-butttons">
+                    <div className="actions">
+                        <PlayCircleFilled className="play" fontSize="large"></PlayCircleFilled>
+                    </div>
+                    <div className="song-bar">
+                        <div>0:00</div>
+                        <Slider className="progress-bar"/>
+                        <div>4:00</div>
+                    </div>
+                </div>
+                <div className="options">
+                    <VolumeUp></VolumeUp>
+                    <Slider className="volume"/>
+                </div>
             </div>
         )
     }
