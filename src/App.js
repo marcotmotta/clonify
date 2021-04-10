dfca4744084c237a66c97e0c6d5d301081b10924
Marco Túlio <marcotmotta@gmail.com>
2021-04-09 21:51:52 -0300

    new display for app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.scss';
 
+import LeftBar from "./components/LeftBar/LeftBar"
+import RightBar from "./components/RightBar/RightBar"
+import Player from "./components/Player/Player"
+
 import Home from "./components/Home/Home"
 
 function App() {
-  return (
-    <div className="App">
-      <Home ></Home>
-    </div>
-  );
+    // Every component must return one and only one div
+    return (
+        <div className="App">
+            <div className="content">
+                <LeftBar></LeftBar>
+                <Home ></Home>
+                <RightBar></RightBar>
+            </div>
+            <Player></Player>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,43 +2,33 @@ import React, { Component } from 'react'
 
 import './Home.scss';
 
-import LeftBar from "./../LeftBar/LeftBar"
-import RightBar from "./../RightBar/RightBar"
-import Player from "./../Player/Player"
-
 import { Card, CardActionArea, CardContent, CardMedia } from '@material-ui/core';
 
 export default class Home extends Component {
     render() {
         return (
-            // Every component must return one and only one div
             <div className="home">
-                <LeftBar></LeftBar>
-                <div className="content">
-                   {/*  <div className="card">
-                        <div className="test"></div>
-                        <div className="title">
-                            <div><strong>Blinding Lights</strong></div>
-                            <div>The Weeknd</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="test"></div>
-                        <div className="title">
-                            <div><strong>Blinding Lights</strong></div>
-                            <div>The Weeknd</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="test"></div>
-                        <div className="title">
-                            <div><strong>Blinding Lights</strong></div>
-                            <div>The Weeknd</div>
-                        </div>
-                    </div> */}
-                </div>
-                <RightBar></RightBar>
-                <Player></Player>
+                {/*  <div className="card">
+                     <div className="test"></div>
+                     <div className="title">
+                         <div><strong>Blinding Lights</strong></div>
+                         <div>The Weeknd</div>
+                     </div>
+                 </div>
+                 <div className="card">
+                     <div className="test"></div>
+                     <div className="title">
+                         <div><strong>Blinding Lights</strong></div>
+                         <div>The Weeknd</div>
+                     </div>
+                 </div>
+                 <div className="card">
+                     <div className="test"></div>
+                     <div className="title">
+                         <div><strong>Blinding Lights</strong></div>
+                         <div>The Weeknd</div>
+                     </div>
+                 </div> */}
             </div>
         )
     }
